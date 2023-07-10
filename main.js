a671e695c4c381fce93e277976a32af4fe33edb2
Anthony Lotson <focus_and_finish@yahoo.com>
2023-07-10 14:55:05 -0400

    added focus event listener to form lines 7-9

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("apple-sauce")
+console.log("main.js connected");
+
+const handleFormInputFocus = () => {
+    console.log("focus occurred");
+};
+
+const searchTermsInput = document.body.querySelector("#search-terms");
+
+searchTermsInput.addEventListener("focus", handleFormInputFocus)
