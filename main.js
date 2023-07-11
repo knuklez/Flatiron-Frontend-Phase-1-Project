7e6205490ac8893acb405a648b53ceaaa54d9e39
Anthony Lotson <focus_and_finish@yahoo.com>
2023-07-11 19:55:14 -0400

    added api connect to framework

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
 console.log("main.js connected");
 
-const handleFormInputFocus = () => {
-    console.log("focus occurred");
-};
 
 const searchTermsInput = document.body.querySelector("#search-terms");
+
+//adding and querying api
+
+const getMealCategories = async () => {
+    const mealCategoriesApiURL = "https://www.themealdb.com/api/json/v1/1/categories.php"
+
+    //need to do a fetch = review "await"
+
+    const response = await fetch(mealCategoriesApiURL);
+
+    const data = response.json()
+
+    console.log(data)
+
+    //const data = response.json converts response into a json format - like saving a letter as a ".doc" file.  can use as a json file at this point
+
+    //const data = response.json() added after verifying after fetch worked
+
+    //console.log(response) use to see if working (it is working) 
+
+    //console.log(data) to see response in a json format
+    
+}
+// review purpose of "async"
+const handleFormInputFocus = async () => {
+    console.log("focus occurred");
+
+    await getMealCategories();
+};
 
 searchTermsInput.addEventListener("focus", handleFormInputFocus)
