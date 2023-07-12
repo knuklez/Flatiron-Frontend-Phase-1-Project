60e65008b6963689071bcd527bacaf9727dd1929
Anthony Lotson <focus_and_finish@yahoo.com>
2023-07-12 03:19:21 -0400

    added try/catch + converted data into a json + cleaned up and insured all awaits were properly added to avoid asynch issues

--- main.js
+++ main.js
@@ -8,28 +8,47 @@ const searchTermsInput = document.body.querySelector("#search-terms");
 const getMealCategories = async () => {
     const mealCategoriesApiURL = "https://www.themealdb.com/api/json/v1/1/categories.php"
 
+    //try/catch is an error checking/catching method (check mdn).  see the location of the code.  has been moved inside of the try/catch - I left the original code outside of the try/catch.
+
+    /* **********try/catch example using code from elswhere in the file/
+    try {
+    const response = await fetch(mealCategoriesApiURL);
+    const data = response.json()
+    console.log("data:", data); //data variable must be defined inside of the try/catch
+    } catch (error) {
+     console.log(error)
+     alert("something went wrong, try again later")   
+    }
+    */ 
+
     //need to do a fetch = review "await"
 
-    const response = await fetch(mealCategoriesApiURL);
+    const response = await fetch(mealCategoriesApiURL); //if use await here then also use on line 30 - const data = await response.json()
 
-    const data = response.json()
+    //could use "then" method instead of "await" method by adding it to the "fetch" - const response = await fetch(mealCategoriesApiURL).then(response => json())
 
-    console.log(data)
+    //const data = response.json()  //needed to add await as line 26 is asynch but line 30 is Notification.  only getting a promise as output in live code
+
+    const data = await response.json()
+
+    //console.log("data: ", data) //tested and used instead of console.log(data)
 
     //const data = response.json converts response into a json format - like saving a letter as a ".doc" file.  can use as a json file at this point
 
     //const data = response.json() added after verifying after fetch worked
 
-    //console.log(response) use to see if working (it is working) 
+    //console.log(response) use to see if working (it is working) - tested
 
-    //console.log(data) to see response in a json format
+    //console.log(data) to see response in a json format - tested
     
 }
 // review purpose of "async"
+
+
 const handleFormInputFocus = async () => {
     console.log("focus occurred");
 
     await getMealCategories();
 };
-
 searchTermsInput.addEventListener("focus", handleFormInputFocus)
+
